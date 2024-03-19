ecd2e88105f9d70e4e4bc8c2604e1a6f89db411b
MaksymStarodubtsev1 <88541521+MaksymStarodubtsev1@users.noreply.github.com>
2024-03-19 18:37:36 +0200

    server: code style config

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,15 @@ module.exports = {
          "ecmaVersion": 12,
          "sourceType": "module"
     },
-    "extends": ["eslint:recommended", "prettier"],
+    "extends": [
+     "airbnb-base",
+     "airbnb-typescript/base",
+     "plugin:@typescript-eslint/recommended",
+     "plugin:eslint-comments/recommended",
+     "plugin:jest/recommended",
+     "plugin:promise/recommended",
+     "prettier"
+     ],
     "env": { 
          "es2021": true,
          "node": true
@@ -14,6 +22,35 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
-         "no-console": "error"
-    }
+     "prettier/prettier": "error",
+     "import/prefer-default-export": "off",
+     "import/no-default-export": "error",
+     "no-use-before-define": [
+         "error",
+         {
+             "functions": false,
+             "classes": true,
+             "variables": true
+         }
+     ],
+     "@typescript-eslint/explicit-function-return-type": "off",
+     "@typescript-eslint/no-use-before-define": [
+         "error",
+         {
+             "functions": false,
+             "classes": true,
+             "variables": true,
+             "typedefs": true
+         }
+     ],
+     "import/no-extraneous-dependencies": "off"
+ },
+ "settings": {
+     "import/resolver": {
+         "typescript": {
+             "alwaysTryTypes": true,
+             "project": "./tsconfig.json"
+         }
+     }
+ }
 }
--- index.ts
+++ index.ts
@@ -1,14 +1,25 @@
-import express, { Express, Request, Response , Application } from 'express';
+import express, { Express, Request, Response, Application } from 'express';
 import dotenv from 'dotenv';
 
-//For env File 
+import { PrismaClient } from '@prisma/client';
+
+//For env File
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
+const prisma = new PrismaClient();
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
+});
+
+app.post('/column', (req: Request, res: Response) => {
+  try {
+    if (req.body.id) {
+    }
+  } catch (err) {}
 });
 
 app.listen(port, () => {
