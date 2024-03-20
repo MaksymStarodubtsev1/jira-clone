238314605ad6f288e77c26559a4800a86d4262df
MaksymStarodubtsev1 <88541521+MaksymStarodubtsev1@users.noreply.github.com>
2024-03-20 22:35:25 +0200

    server: added delete card api

--- index.ts
+++ index.ts
@@ -24,6 +24,7 @@ app.delete('/board/:id', BoardController.remove);
 
 app.post('/card', CardController.create);
 app.patch('/card/:id', CardController.update);
+app.delete('/card/:id', CardController.remove);
 
 app.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
